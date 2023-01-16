863bd31066822e27950aafd494a0c23a05b658f2
Anelda van der Walt <anelda@talarify.co.za>
2023-01-16 11:15:55 +0200

    Created tabs for different outputs and added About page

--- app.R
+++ app.R
@@ -41,15 +41,51 @@ readr::write_csv(mapping_w_coords, here("data", "mapping_w_coords.csv"))
 
 ui <- fluidPage(
   titlePanel(p("Research Software Initatives in Africa", style = "color:#3474A7")),
-  leafletOutput(outputId = "map"),
-              DTOutput(outputId = "table")
+  
+  
+  tabsetPanel(type = "tabs",
+              tabPanel("Map",  leafletOutput(outputId = "map")),
+              tabPanel("Table", DTOutput(outputId = "table")),
+              tabPanel("About", 
+                   HTML(r"(
+                        <p>This app was developed by Nomalungelo Maphanga and Anelda van der Walt from <a href=\"https://talarify.co.za\">Talarify</a> 
+                        as part of a project to create more awareness about the breadth of interest and expertise in research software in Africa.
+                       </p>
+                       <p>Data included in this visualisation were sourced as follows:
+                       <ul>
+                        <li> The Research Software Alliance <a href=\"https://www.researchsoft.org/\">(ReSA)</a> recruited 
+                        contracters to perform a mapping of research software initiatives in the Global South in 2021/2022;</li>
+                        <li> Talarify built on the work by ReSA in 2022/2023 through web searches, outreach to their networks, 
+                              and outreach at the World Science Forum to collect more data.</li>
+                      </ul>
+                     </p>
+                     <p>The additional mapping and development of the Shiny app was run as part of Cohort 6 of the Open Life 
+                     Science Mentoring and Training Programme <a href=\"https://openlifesci.org/\">(OLS)</a>.
+                     </p>
+                     
+                     <p>For more information about the ReSA mapping, please <a href=\"https://www.researchsoft.org/blog/2022-10/\">read the 
+                     blog post</a> by Michelle Barker.
+                     </p>
+                     
+                     <p>
+                     The additional mapping is a work in progress and is currently available in 
+                     <a href=\"https://docs.google.com/spreadsheets/d/18FSidlJ4o1AOwz7lVoy2A8iWDxiADHMZ4sWMEIisYJA/edit#gid=0\">this Google Sheet</a>.
+                     Please address any questions about this work to <a href=\"mailto:anelda@talarify.co.za\">Anelda van der Walt</a>. 
+                     The project was funded by Talarify.
+                     </p>
+                     )"
+                   )
+                   )
+              )
+              
 )
 
 # SHINY APP SERVER ----
 
 server <- function(input, output){
   
-  output$table <- renderDT(mapping_w_coords)
+  output$table <- renderDT(mapping)
+  
   
   output$map <- renderLeaflet({
     
@@ -59,8 +95,9 @@ server <- function(input, output){
       addAwesomeMarkers(lng=~Longitude, lat=~Latitude,
                         popup=~paste("Type:", Type),
                         label=~paste0(Name, "(", Country, ")"),
-                        clusterOptions = markerClusterOptions())
-
+                        clusterOptions = markerClusterOptions()
+                        )
+  
     rse_map
 
   })
